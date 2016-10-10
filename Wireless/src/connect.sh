bf91eb7663917f6b8eec9f5ea88e9c4131850088
BlackDragonF <574712047@qq.com>
2016-10-10 13:34:13 +0800

    Fixed bug with Wi-Fi interface and add comment

--- Wireless/src/connect.sh
+++ Wireless/src/connect.sh
@@ -1,7 +1,22 @@
 query="{query}"
-
+#Get curent Wi-Fi interface
+interface=$(networksetup -listallhardwareports | grep -C 1 Wi-Fi | tail -n 1 | awk -F ':' '{print $NF}')
+#Get SSID
 name=$(echo $query | awk -F ' ' '{printf("%s", $1)}{for (i = 2 ; i < NF ; i++) printf(" %s", $i)}')
-
+#Get Password
 password=$(echo $query | awk -F ' ' '{printf("%s", $NF)}')
 
-networksetup -setairportnetwork en0 "$name" "$password"
+#Check Wi-Fi interface
+if [$interface == '']
+then
+    echo "No Wi-Fi interface found"
+else
+    #Check Wi-Fi power
+    power=$(networksetup getairportpower en0 | awk -F ' ' '{print $NF}')
+    if [$power == 'Off']
+    then
+        echo "Current Wi-Fi interface is off"
+    else
+        networksetup -setairportnetwork $interface "$name" "$password"
+    fi
+fi
--- Wireless/src/scan.py
+++ Wireless/src/scan.py
@@ -7,34 +7,43 @@ from xml.etree.ElementTree import Element, SubElement, tostring
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
+#Call built-in airport command
 cmd_results = os.popen("/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -s").read().split("\n")
 if len(cmd_results)>1:
-    header=cmd_results.pop(0)
+    header = cmd_results.pop(0)
     cmd_results.pop()
 else:
     exit()
 
-formatter={'SSID_end':header.find('SSID')+4, 'RSSI_beg':header.find('RSSI'), 'RSSI_end':header.find('CHANNEL')-1, 'SECURITY_beg':header.find('SECURITY')}
+#Extract information from output
+formatter = {
+        'SSID_end':     header.find('SSID')+4,
+        'BSSID_beg':    header.find('BSSID'),
+        'BSSID_end':    header.find('RSSI')-1,
+        'RSSI_beg':     header.find('RSSI'),
+        'RSSI_end':     header.find('CHANNEL')-1,
+        'SECURITY_beg': header.find('SECURITY')
+    }
 
-output_results=[]
+output_results = []
 
 for item in cmd_results:
-    output_result={}
-    output_result['SSID']=item[0:formatter['SSID_end']].strip()
-    output_result['RSSI']=item[formatter['RSSI_beg']:formatter['RSSI_end']].strip()
-    output_result['SECURITY']=item[formatter['SECURITY_beg']:].strip()
+    output_result = {}
+    output_result['SSID']        =      item[0:formatter['SSID_end']].strip()
+    output_result['BSSID']       =      item[formatter['BSSID_beg']:formatter['BSSID_end']].strip()
+    output_result['RSSI']        =      item[formatter['RSSI_beg']:formatter['RSSI_end']].strip()
+    output_result['SECURITY']    =      item[formatter['SECURITY_beg']:].strip()
     output_results.append(output_result)
 
-items=Element('items')
-uid = 0;
+#Generate XML
+items = Element('items')
 for result in output_results:
-    item=SubElement(items, 'item', {'autocomplete':result['SSID'], 'uid':str(uid), 'arg':result['SSID']})
-    title=SubElement(item, 'title')
-    title.text=result['SSID']
-    subtitle=SubElement(item, 'subtitle')
-    subtitle.text='RSSI:'+result['RSSI']+'\t\t\t\t'+'Security:'+result['SECURITY']
-    icon=SubElement(item, 'icon')
-    icon.text='icon.png'
-    uid=uid+1
+    item = SubElement(items, 'item', {'autocomplete':result['SSID'], 'uid':result['BSSID'], 'arg':result['SSID']})
+    title = SubElement(item, 'title')
+    title.text = result['SSID']
+    subtitle = SubElement(item, 'subtitle')
+    subtitle.text = 'RSSI:'+result['RSSI']+'\t\t\t\t'+'Security:'+result['SECURITY']
+    icon = SubElement(item, 'icon')
+    icon.text = 'icon.png'
 
 print tostring(items)
