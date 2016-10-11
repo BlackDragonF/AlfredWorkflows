5b68a9fe534448c3809fc4ae3b1002a0e6a2a000
BlackDragonF <574712047@qq.com>
2016-10-11 11:28:02 +0800

    Fixed bug with connect script in bash and awk

--- Wireless/src/connect.sh
+++ Wireless/src/connect.sh
@@ -1,19 +1,19 @@
 query=$1
 #Get curent Wi-Fi interface
-interface=$(networksetup -listallhardwareports | grep -C 1 Wi-Fi | tail -n 1 | awk -F ':' '{print $NF}')
+interface=$(networksetup -listallhardwareports | grep -C 1 Wi-Fi | tail -n 1 | awk -F ' ' '{print $NF}')
 #Get SSID
 name=$(echo $query | awk -F ' ' '{printf("%s", $1)}{for (i = 2 ; i < NF ; i++) printf(" %s", $i)}')
 #Get Password
 password=$(echo $query | awk -F ' ' '{printf("%s", $NF)}')
 
 #Check Wi-Fi interface
-if [$interface = '']
+if [ $interface = '' ]
 then
     echo "No Wi-Fi interface found"
 else
     #Check Wi-Fi power
     power=$(networksetup getairportpower $interface | awk -F ' ' '{print $NF}')
-    if [$power = 'Off']
+    if [ $power = 'Off' ]
     then
         echo "Current Wi-Fi interface is off"
     else
